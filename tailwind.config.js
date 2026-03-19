f687abe544fce8addd89a90b38934286981ae0da
Nourasha <69981085+Nourasha@users.noreply.github.com>
2026-03-19 12:10:53 +0100

    Add safelist for day card background colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  safelist: ["bg-gray-50", "bg-blue-50", "text-blue-800", "text-gray-800"],
   theme: {
     extend: {},
   },
