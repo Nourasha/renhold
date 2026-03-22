64d66d42165f974199017bd989bca30d39eb021a
Nourasha <69981085+Nourasha@users.noreply.github.com>
2026-03-22 17:27:52 +0100

    Fix Railway NEXTAUTH trust host

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.VAPID_PUBLIC_KEY,
   },
+  experimental: {
+    trustHostHeader: true,
+  },
 };
 module.exports = nextConfig;
